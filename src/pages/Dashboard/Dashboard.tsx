e0e72b1b1847ac3063c8137f1d9d340d618bb473
HoangLogivan <hoangnd@logivan.com>
2022-01-11 17:55:36 +0700

    get data from coingeko(1)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { rechartLineData } from 'components/Dashboard/data';
 import { styled } from '@mui/material/styles';
 import Statistics from 'components/Dashboard/Statistics';
