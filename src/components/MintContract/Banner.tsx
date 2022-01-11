86112da5f3e5253cd0da3d5d00cf600266e741c0
Huy Le <mrsjiodev@gmail.com>
2022-01-11 16:37:13 +0700

    fix gif icon

---
+++ src/components/MintContract/Banner.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+interface Props {
+  title?: string;
+}
+
+const Banner: React.FC<Props> = () => {
+  return <Box>aaaa</Box>;
+};
+
+export default Banner;
