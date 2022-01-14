0e803ae69dedd7fc286bc503701ca91f67d2f022
HoangLogivan <hoangnd@logivan.com>
2022-01-14 13:59:40 +0700

    remove call detail aave

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -4,7 +4,7 @@ import Statistics from 'components/Dashboard/Statistics';
 import TotalMinted from 'components/Dashboard/TotalMinted';
 import PriceChart from 'components/Dashboard/PriceChart';
 import { Box, Grid, ToolbarProps, Toolbar, Typography } from '@mui/material';
-import { coinsDetail, coinsHistory } from 'services/coingeko';
+import { coinsHistory } from 'services/coingeko';
 import { useAppDispatch, useAppSelector } from 'stores/hooks';
 
 interface DashboardProps {
@@ -23,11 +23,15 @@ const Dashboard: React.FC<DashboardProps> = () => {
   // const aave = useAppSelector((state) => state.coingeko.detail);
   const history: any = useAppSelector((state) => state.coingeko.history);
   useEffect(() => {
-    dispatch(coinsDetail('aave'));
+    // dispatch(coinsDetail('aave'));
     dispatch(coinsHistory('aave', { vs_currency: 'usd', days: '364' }));
   }, []);
   const rechartLineData = (history?.prices || []).map((el: any) => {
-    return { time: el[0], close: el[1] };
+    return {
+      time: el[0],
+      close: el[1],
+      conversionSymbol: '',
+    };
   });
 
   return (
