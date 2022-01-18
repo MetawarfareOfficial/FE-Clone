a2b2b92f2a28090c68feb65f5d799b102d659bf4
Sotatek-PhucNguyen2 <phuc.nguyen2@sotatek.com>
2022-01-18 14:00:09 +0700

    feat: add interval to price chart

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import PriceChart from 'components/Dashboard/PriceChart';
 import { Box, Grid } from '@mui/material';
 import { coinsPrices } from 'services/coingeko';
 import { useAppDispatch, useAppSelector } from 'stores/hooks';
+import useInterval from 'hooks/useInterval';
 
 interface DashboardProps {
   name?: string;
@@ -13,15 +14,22 @@ interface DashboardProps {
 const Dashboard: React.FC<DashboardProps> = () => {
   const dispatch = useAppDispatch();
   const data = useAppSelector((state) => state.coingeko.data);
+
   useEffect(() => {
     dispatch(coinsPrices({ vs_currency: 'usd', days: '365' }));
   }, []);
+
+  useInterval(async () => {
+    await dispatch(coinsPrices({ vs_currency: 'usd', days: '365' }));
+  }, 50000);
+
   const rechartLineData = (data?.prices || []).map((el: Array<string>) => {
     return {
       time: el[0],
       close: el[1],
     };
   });
+
   return (
     <Box>
       <Box mt="30px" mb="50px">
