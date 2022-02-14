64a84f7e4f6d2d7fd74c1201247f1779bbf1d634
dohainam <nam.do2@sotatek.com>
2022-02-14 18:01:04 +0700

    fix: fix bug format reward wrong for number less than 100

--- src/helpers/formatReward.ts
+++ src/helpers/formatReward.ts
@@ -1,6 +1,13 @@
 import { calculateToFixedNumber } from './calculateToFixedNumber';
 import { formatBigNumber } from './formatBigNumber';
 
+const convertLessThanThreeNumberValue = (value: string) => {
+  if (value.length === 2) {
+    return value + '.00';
+  }
+  return value + '.000';
+};
+
 export const formatReward = (value: string, isAbbreviated = true) => {
   if (value === 'null' || value === 'undefined') {
     return '0';
@@ -9,6 +16,8 @@ export const formatReward = (value: string, isAbbreviated = true) => {
   const afterPeriodNumber = value.split('.')[1];
   if (beforePeriodNumber.length > 3) {
     return `${formatBigNumber(Number(beforePeriodNumber), isAbbreviated, false)} `;
+  } else if (beforePeriodNumber.length < 3 && !afterPeriodNumber) {
+    return convertLessThanThreeNumberValue(beforePeriodNumber);
   } else if (afterPeriodNumber) {
     return Number(value).toFixed(calculateToFixedNumber(value, false));
   }
