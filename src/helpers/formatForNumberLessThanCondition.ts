5c36bb178ace6099c9a87ae1c829702a8fdce612
dohainam <nam.do2@sotatek.com>
2022-02-15 17:32:23 +0700

    fix: fix format my reward

--- src/helpers/formatForNumberLessThanCondition.ts
+++ src/helpers/formatForNumberLessThanCondition.ts
@@ -1,5 +1,7 @@
 export const formatForNumberLessThanCondition = (value: number | string, numberCondition: number | string) => {
-  if (Number(value) < Number(numberCondition)) {
+  if (Number(value) === 0) {
+    return value;
+  } else if (Number(value) < Number(numberCondition)) {
     return '<' + String(numberCondition);
   }
   return value;
