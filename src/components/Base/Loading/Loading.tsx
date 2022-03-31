24b1c16906a98b395315c501bf653668f2112124
nam.do2@sotatek.com <dohainamhn2@gmail.com>
2022-03-31 13:58:43 +0700

    fix: change background of status modal in dark mode to white

--- src/components/Base/Loading/Loading.tsx
+++ src/components/Base/Loading/Loading.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import Lottie from 'react-lottie';
 
 import animationData from 'lotties/loading.json';
+import { Box, BoxProps, styled } from '@mui/material';
 
 interface Props {
   loading?: boolean;
 }
-
+const AnimationBox = styled(Box)<BoxProps>(() => ({
+  '& div:first-child': {
+    background: '#fff',
+  },
+}));
 const Loading: React.FC<Props> = () => {
   const defaultOptions = {
     loop: true,
@@ -18,9 +23,9 @@ const Loading: React.FC<Props> = () => {
   };
 
   return (
-    <div>
+    <AnimationBox>
       <Lottie options={defaultOptions} height={200} width={200} />
-    </div>
+    </AnimationBox>
   );
 };
 
