0a1f0aa768afbe0b358008fb152915dffde1a673
nam.do2@sotatek.com <dohainamhn2@gmail.com>
2022-04-09 11:57:30 +0700

    fix conflict

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -694,15 +694,11 @@ const Layout: React.FC<Props> = ({ children }) => {
                           ))}
                       </div>
                     </div>
-                    {/* </SliderScroll> */}
                   </MenusMobile>
 
-                  {
-                    // location.pathname !== '/treasury' &&
-                    width > 899 && (
-                      <Banner isBg={location.pathname === '/' || location.pathname === '/treasury' ? false : true} />
-                    )
-                  }
+                  {width > 899 && (
+                    <Banner isBg={location.pathname === '/' || location.pathname === '/treasury' ? false : true} />
+                  )}
 
                   {children}
                 </MainLayout>
