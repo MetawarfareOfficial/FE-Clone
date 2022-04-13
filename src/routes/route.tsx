f95a62e73e797e06f4865f995c96ff551eef9638
Sotatek-ThuyetNguyen <thuyet.nguyen@sotatek.com>
2022-04-13 15:35:11 +0700

    Update: add comment

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -38,9 +38,10 @@ const routers = {
     component: SwapPage,
     route: PrivateRoute,
   },
+  // Hide zap page
   // zap: {
   //   exact: true,
-  //   path: routesConst.ZAP,  // hide zap page
+  //   path: routesConst.ZAP,
   //   component: ZapPage,
   //   route: PrivateRoute,
   // },
