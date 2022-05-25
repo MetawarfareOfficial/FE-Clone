2f21916df5b1a32cd92c797fbe55b61016edfa5b
nam.do2@sotatek.com <dohainamhn2@gmail.com>
2022-05-25 12:07:31 +0700

    fix due days are wrong

--- src/helpers/myContract/calculateDueDate.ts
+++ src/helpers/myContract/calculateDueDate.ts
@@ -1,11 +1,13 @@
 import moment from 'moment';
 
-export const calculateDueDate = (expiredTime: number) => {
+export const calculateDueDate = (expiredTime: number, _oneMonthTime: number) => {
   const now = moment();
+  const oneDaySecond = 60 * 60 * 24;
   const remainHours = moment(expiredTime * 1000).diff(now, 'h');
   const remainDays = Math.ceil(remainHours / 24);
-  if (remainDays > 30) {
-    return remainDays - 30;
+  const oneMonthTime = Math.ceil(_oneMonthTime / oneDaySecond);
+  if (remainDays >= oneMonthTime) {
+    return remainDays - oneMonthTime;
   }
   return remainDays;
 };
